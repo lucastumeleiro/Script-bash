1817ffc33855bfc51ae49093c346267dd6fe69d7
Lucas Tumeleiro <49693585+lucastumeleiro@users.noreply.github.com>
2019-10-24 21:07:49 -0300

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,33 +1,25 @@
 #!/bin/bash
-#__BEGIN_MAIN__
-int resp = 0;
-while (resp <> 6){
+resp=0
+while [$resp -ne 6]
+do
 echo -------------------------------------------------------------------------------------------
-echo “1 - Criar conta de usuário”
-echo “2 - Alterar senha do usuário”
-echo “3 - Remover usuário”
-echo “4 - Ver dados da conta do usuário”
-echo “5 - Alterar dados do usuário”
-echo “6 - Sair”
+echo "1 - Criar conta de usuário"
+echo "2 - Alterar senha do usuário"
+echo "3 - Remover usuário"
+echo "4 - Ver dados da conta do usuário"
+echo "5 - Alterar dados do usuário"
+echo "6 - Sair"
 echo -------------------------------------------------------------------------------------------
-printf("Escolha uma opção:");
-scanf("%f",&resp);
-	switch (resp){
-		case 1: printf("Opção 1");
-        break;
-		case 2: printf("Opção 2");
-        break;
-		case 3: printf("Opção 3");
-        break;
-		case 4: printf("Opção 4");
-        break;
-		case 5: printf("Opção 5");
-        break;
-		case 6: printf("Opção 6");
-        break;
-        default: printf("Opção Inválida!");
-	}
+read -p "Escolha uma opção:" $resp
 
-}
-#__END_MAIN__
+	case $resp in
+		1) echo "Opção 1" ;;
+		2) echo "Opção 2" ;;
+		3) echo "Opção 3" ;;
+        4) echo "Opção 4" ;;
+        5) echo "Opção 5" ;;
+        6) echo "Opção 6" ;;
+        *) echo "Opção Inválida!" ;;
+	esac
 
+done
